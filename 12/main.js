26a2172cbdc134e6843f7afc3521f87ab970e2b6
sezerweb <sezerakbulut@gmail.com>
2021-09-07 12:23:47 +0300

    12 bitti

--- 12/main.js
+++ 12/main.js
@@ -1,8 +1,24 @@
-function showPopover(){
-   
-    document.getElementById("popover").style.display = "block";
+function docReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+docReady(function() {
+    if (!"speechSynthesis" in window) {
+        var speakerObj = document.getElementById('speakerIcon') ;
+        speakerObj.style.display = 'none';
+    }
+});
+
+function speech(text) {
+    var to_speak = new SpeechSynthesisUtterance(text);
+    to_speak.rate = 1.4;
+    to_speak.lang = 'en-US';
+    window.speechSynthesis.speak(to_speak);
 }
 
-function hidePopover(){
-    document.getElementById("popover").style.display = "none";
-}
+
+
+
